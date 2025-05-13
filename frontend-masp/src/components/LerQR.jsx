bd4cc640d54e00029e6d077e440dce096356a690
drumondpe <pedrogsd@al.insper.edu.br>
2025-05-13 12:34:57 -0300

    diminuindo quadrado

--- frontend-masp/src/components/LerQR.jsx
+++ frontend-masp/src/components/LerQR.jsx
@@ -20,7 +20,7 @@ export default function LerQR({ onScanResult, onClose }) {
         { facingMode: "environment" },
         {
           fps: 10,
-          qrbox: { width: 250, height: 250 },
+          qrbox: { width: 150, height: 150 },
           aspectRatio: 1.0, // ✅ quadrado
         },
         (decodedText) => {
